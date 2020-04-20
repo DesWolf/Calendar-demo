b7ec2698db87ed543e66246599bcfb85d4887978
DesWolf <okuneev@gmail.com>
2020-04-20 20:50:18 +0300

    <commit message>

--- Calendar demo/ViewController.swift
+++ Calendar demo/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import CVCalendar
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    @IBOutlet weak var calendarView: CVCalendarView!
+    @IBOutlet var dayTextView: UITextView!
+    
+    var datesDictionary:[String:String] = ["20 April, 2020":"Service","19 April, 2020":"Change Oil","21 April, 2020":"Check brakes"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendarView.delegate = self
+        menuView.delegate = self
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        menuView.commitMenuViewUpdate()
+        calendarView.commitCalendarViewUpdate()
+    }
+    
+    
 }
+extension ViewController: CVCalendarMenuViewDelegate, CVCalendarViewDelegate {
+    func presentationMode() -> CalendarMode {
+        return CalendarMode.monthView
+    }
+    
+    func firstWeekday() -> Weekday {
+        return Weekday.monday
+    }
+    
+    func dotMarker(shouldShowOnDayView dayView: DayView) -> Bool{
+        // Look up date in dictionary
+        if(datesDictionary[dayView.date.commonDescription] != nil){
+            return true // date is in the array so draw a dot
+        }
+        return false
+    }
+    func dotMarker(colorOnDayView dayView: DayView) -> [UIColor]{
+        return [UIColor.black]
+    }
+//    func dotMarker(sizeOnDayView dayView: DayView) -> CGFloat {
+//        return 
+//    }
 
+    
+    func didSelectDayView(_ dayView: DayView, animationDidFinish: Bool){
+        
+        dayTextView.text = ""
+        // Look up date in dictionary
+        if(datesDictionary[dayView.date.commonDescription] != nil){
+            dayTextView.text = datesDictionary[dayView.date.commonDescription] // day is in the dictionary - wrote the corresponding text to dayTextView
+        }
+    }
+    
+    
+    
+}
