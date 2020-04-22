9047a1fcbef70f094e18bf28e409e4598a263147
DesWolf <okuneev@gmail.com>
2020-04-22 21:38:31 +0300

    add maya

--- Calendar demo/Contacts/ContactsTVC.swift
+++ Calendar demo/Contacts/ContactsTVC.swift
@@ -7,31 +7,72 @@
 //
 
 import UIKit
+import Moya
 
 class ContactsTVC: UITableViewController {
 
-    var users = [Users]()
+    var users = [User]()
+    let userProvider = MoyaProvider<UserService>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        userProvider.request(.readUsers) { (result) in
+            switch result {
+            case .success(let response):
+                let users = try! JSONDecoder().decode([User].self, from: response.data)
+                self.users = users
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     @IBAction func didTapAdd() {
-        let kilo = Users(id: 55, name: "Kilo Loco")
-        print(kilo)
+        let kilo = User(id: 55, name: "Kilo Loco")
+        
+        userProvider.request(.createUser(name: kilo.name)) { (result) in
+            switch result {
+            case .success(let response):
+                let newUser = try! JSONDecoder().decode(User.self, from: response.data)
+                self.users.insert(newUser, at: 0)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
    
-    numberOfRowInSection
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = users[indexPath.row]
+        
+        userProvider.request(.updateUser(id:user.id, name:"[Modified] " + user.name)) { (result) in
+            switch result {
+            case .success(let response):
+                let modifiedUser = try! JSONDecoder().decode(User.self, from: response.data)
+                self.users[indexPath.row] = modifiedUser
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        let user = users[indexPath.row]
+        print(user)
+    }
 }
