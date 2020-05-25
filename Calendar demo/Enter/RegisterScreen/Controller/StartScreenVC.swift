b7a253f2be33a272f88012957961f4f4d029e423
DesWolf <okuneev@gmail.com>
2020-05-25 19:50:45 +0300

    refactoring

--- Calendar demo/Enter/RegisterScreen/Controller/StartScreenVC.swift
+++ Calendar demo/Enter/RegisterScreen/Controller/StartScreenVC.swift
@@ -6,47 +6,13 @@
 //  Copyright © 2020 Максим Окунеев. All rights reserved.
 //
 //
-//import UIKit
-//
-//class StartScreenVC: UIViewController {
-//
-//    @IBOutlet weak var emailTF: UITextField!
-//    @IBOutlet weak var passwordTF: UITextField!
-//
-//    private var email: String = "ok@mail.ru"
-//    private var password: String = "Aa12345678"
-//    private let networkManagerLogin = NetworkManagerLogin()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//    }
-//
-//    @IBAction func pressEnterButton(_ sender: Any) {
-//        loginUser()
-//    }
-//
-//}
-//
-////MARK: Network
-//extension StartScreenVC {
-//    private func loginUser() {
-//        networkManagerLogin.loginUser(email: email, password: password) { [weak self] (message, error)  in
-//            guard let message = message else {
-//                print(error ?? "")
-//                    DispatchQueue.main.async {
-//                        self?.alertNetwork(message: error ?? "")
-//                    }
-//                    return
-//                }
-//            print(message)
-//        }
-//    }
-//}
-//
-////MARK: Alert
-//extension StartScreenVC  {
-//    func alertNetwork(message: String) {
-//        UIAlertController.alert(title:"Error", msg:"\(message)", target: self)
-//    }
-//}
+import UIKit
+
+class EnterScreenVC: UIViewController {
+
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
